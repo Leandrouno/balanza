0634accc0e4386337aa7381628efda03d663995a
Leandrouno <lmugnolo@gmail.com>
2018-08-30 18:17:03 -0300

    Update balanza.ino

--- balanza.ino
+++ balanza.ino
@@ -6,6 +6,9 @@
 
 #define DOUT  A1
 #define CLK  A0
+#define SET 2
+#define RESET 3
+
 
 HX711 balanza(DOUT, CLK);
 int esta_contando = 0, bandera_inicio = 0, guarda_peso = 0, apagado = 0;
@@ -14,8 +17,8 @@ long anteriores = 0;
 LiquidCrystal_I2C lcd(0x3F,16,2); 
 
 void setup() {
-              pinMode(2,INPUT_PULLUP);
-              pinMode(3,INPUT_PULLUP);
+              pinMode(SET,INPUT_PULLUP);
+              pinMode(RESET,INPUT_PULLUP);
               //Serial.begin(9600);
               //Serial.print("Lectura del valor del ADC:  ");
               //Serial.println(balanza.read());
@@ -24,62 +27,51 @@ void setup() {
               lcd.init();
               lcd.backlight();       
               lcd.setCursor(0, 0);
-              lcd.print("Iniciando tara  ");
+              lcd.print("Iniciando Tara  ");
               lcd.setCursor(0, 1);
               lcd.print("Espere...       ");         
             }
 
 void loop() {
-  if(bandera_inicio == 0)
-  {
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Espere...       ");
-     balanza.set_scale(414844.44); // Establecemos la escala
-              balanza.tare(20);  //El peso actual es considerado Tara.
-              lcd.print("Ya puede pesar  ");          
-    bandera_inicio = 1;
-    esta_contando = 0;
-   }
-if(digitalRead(2)==LOW) {esta_contando = 1; guarda_peso = 0;}
-if(digitalRead(3)==LOW) bandera_inicio = 0;
+               if(bandera_inicio == 0){
+                                       lcd.clear();
+                                       lcd.setCursor(0, 0);
+                                       lcd.print("Espere...       ");
+                                       balanza.set_scale(414844.44); // Establecemos la escala
+                                       balanza.tare(20);  //El peso actual es considerado Tara.
+                                       lcd.print("Ya puede pesar  ");          
+                                       bandera_inicio = 1;
+                                       esta_contando = 0;
+                                       }
+                                      if(digitalRead(SET)==LOW) {esta_contando = 1; guarda_peso = 0;}
+                                      if(digitalRead(RESET)==LOW) bandera_inicio = 0;
 
-if(esta_contando == 1)  {                  
-                         if(guarda_peso == 0) {peso = peso_actual; guarda_peso = 1;}
-                         lcd.setCursor(0, 0);
-                         lcd.print("Unidades: ");
-                         lcd.setCursor(10, 0);
-                         lcd.print(peso_actual/peso*10,0);
-                         lcd.print("             ");
-                         }
-                         else
-                            guarda_peso = 0;
+              if(esta_contando == 1)  {                  
+                                       if(guarda_peso == 0) {peso = peso_actual; guarda_peso = 1;}
+                                       lcd.setCursor(0, 0);
+                                       lcd.print("Unidades: ");
+                                       lcd.setCursor(10, 0);
+                                       lcd.print(peso_actual/peso*10,0);
+                                       lcd.print("             ");
+                                      }
+                                     else { guarda_peso = 0; }
                             
- if(millis()-anteriores >= 2000)
- {
-  if(esta_contando == 0)
-  {   
-    lcd.setCursor(0, 0);
-    lcd.print("Pesando...      "); 
-  }
-  else
-    {   
-    lcd.setCursor(15, 0);
-    lcd.print("*"); 
-    }
-  peso_actual = balanza.get_units(20);
-  peso_mostrar = peso_actual*1000;
-  //Serial.print(peso_mostrar,5); Serial.println(" gramos"); 
-  anteriores = millis();
-  if(esta_contando == 0)
-  {   
-  lcd.setCursor(0, 0);
-  lcd.print("             ");                                         
-  }
-  lcd.setCursor(0, 1);
-  lcd.print("Peso: ");
-  lcd.setCursor(6, 1); 
-  lcd.print(peso_mostrar,0);                   
-  lcd.print(" gramos   ");
- }  
+ if(millis()-anteriores >= 1000) {
+                                  lcd.setCursor(15, 0);
+                                  lcd.print("*"); 
+                                  peso_actual = balanza.get_units(5);
+                                  peso_mostrar = peso_actual*1000;
+                                  anteriores = millis();
+                                  
+                                 if(esta_contando == 0)  {   
+                                                          lcd.setCursor(0, 0);
+                                                          lcd.print("                ");                                         
+                                                         }
+                                                          lcd.setCursor(0, 1);
+                                  lcd.print("Peso: ");
+                                  lcd.setCursor(6, 1); 
+                                  lcd.print(peso_mostrar,0);                   
+                                  lcd.print(" gramos   ");
+                                }
+                                  
 }
